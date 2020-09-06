19429473825e4bd0df113d0226549a3542a52ce7
Omer Mohamed Ali <om@envo.me>
2020-09-06 23:35:56 +0200

    Add belongsTo relation

--- src/Definition/Relation.php
+++ src/Definition/Relation.php
@@ -15,13 +15,13 @@ use API\Definition\Base;
 class Relation
 {
     use RuleTrait;
-    
+
     public $key;
-    
+
     public $definition;
-    
+
     public $relationType;
-    
+
     /**
      * Field constructor.
      *
@@ -32,10 +32,10 @@ class Relation
     {
         $this->key = $key;
         $this->definition = $definition;
-        
+
         $this->parse();
     }
-    
+
     public function getRelationTypesSingle()
     {
         return [
@@ -48,7 +48,7 @@ class Relation
             'morphedToMany',
         ];
     }
-    
+
     public function getRelationTypesMultiple()
     {
         return [
@@ -56,50 +56,50 @@ class Relation
             'hasManyThrough'
         ];
     }
-    
+
     public function getData($api, $data, $multiple = false)
     {
         // differentiate between an array of data or one item of data
         $ids = [];
         $items = $multiple ? $data : [$data];
-        
+
         $relationTypes = $this->getRelationTypesSingle();
         $relationTypesMany = $this->getRelationTypesMultiple();
-        
+
         $relationTypesAll = array_merge($relationTypes, $relationTypesMany);
         /** @var API $apiClass */
         $apiClass = app()->get(API::class);
-        
+
         $collection = collect($items);
-        
+
         foreach ($this->rules as $methodName => $rule) {
             if (!in_array($methodName, $relationTypesAll, true)) {
                 continue;
             }
-            
+
             if (!$rule->target) {
                 continue;
             }
-            
+
             /** @var Endpoint $relation */
             $relation = $apiClass->getEndpoint($rule->target);
-            
+
             abort_unless($relation, 500, 'Relation not found');
-            
-            $collectionKeyed = $collection->keyBy($rule->foreign_key);
+
+            $collectionKeyed = $collection->keyBy($rule->type === 'belongsTo' ? $rule->owner_key : $rule->foreign_key);
             //dd($collectionKeyed, $rule);
             $ids = $collectionKeyed->filter()->toArray();
             //dd($ids, $collectionKeyed);
-            
+
             $result = $this->$methodName($api, $rule, $relation, $data, array_keys($ids));
-            
+
             if (!$result) {
                 continue;
             }
-            
+
             $collectionArray = $collectionKeyed->toArray();
             $isMany = in_array($methodName, $relationTypesMany, true);
-            
+
             //dd($result);
             foreach ($result as $id => $item) {
                 if (is_object($item)) {
@@ -115,98 +115,98 @@ class Relation
                     // item reference not found
                     continue;
                 }
-                
+
                 $itemFormatted = $relation->dataHydrate($item);
-                
+
                 if (!isset($dataItem[$this->key])) {
                     $collectionArray[$item[$rule->owner_key]][$this->key] = $isMany ? [] : $itemFormatted;
                 }
-                
+
                 if ($isMany) {
                     $collectionArray[$item[$rule->owner_key]][$this->key][] = $itemFormatted;
                 }
             }
         }
-        
+
         // dd($data, $ids, $collectionArray, $isMany, $result, $this->rules);
-        
+
         return ($collectionArray ?? null) ? array_values($collectionArray) : null;
     }
-    
+
     public function parse()
     {
         $parts = explode('|', $this->definition);
-        
+
         $types = array_merge($this->getRelationTypesSingle(), $this->getRelationTypesMultiple());
         $rules = [];
         foreach ($parts as $raw) {
             $relation = new RelationRule($this->key, $raw);
             $rules[$relation->type] = $relation;
-            
+
             if (in_array($relation->type, $types, true)) {
                 $this->relationType = $relation->type;
             }
         }
-        
+
         $this->rules = $rules;
     }
-    
+
     public function belongsTo(Endpoint $api, RelationRule $rule, Endpoint $relation, $data, $ids)
     {
         if (!$ids ){
             return null;
         }
-        
+
         $builder = DB::table($this->getAPI()->base->getTableName($relation));
-        
+
         $builder->whereIn($rule->owner_key, $ids);
-        
+
         if ($relation->soft_deletes) {
             $builder->whereNull('deleted_at');
         }
-        
+
         $result = $builder->get();
-        
+
         if (!$result) {
             return $result;
         }
-        
+
         return $result->keyBy($rule->owner_key);
     }
-    
+
     public function hasOne(Endpoint $api, RelationRule $rule, Endpoint $relation, $data, $ids)
     {
         $builder = DB::table($relation->getTableName());
-        
+
         $foreignKey = $rule->parameters[1] ?? 'id';
         $ownerKey = $rule->parameters[2] ?? 'id';
         $builder->whereIn($ownerKey, $ids);
-        
+
         if ($relation->soft_deletes) {
             $builder->whereNull('deleted_at');
         }
-        
+
         $result = $builder->get();
-        
+
         if (!$result) {
             return $result;
         }
-        
+
         //dd($result, $foreignKey, $ownerKey, $relation, $rule, $ids, $builder->toSql());
-        
+
         return $result->keyBy($ownerKey);
     }
-    
+
     public function getInfo()
     {
         $rule = reset($this->rules);
         if (!$rule) {
             return [];
         }
-        
+
         return $rule->toArray();
     }
-    
+
     /**
      * @return API
      */
@@ -214,22 +214,22 @@ class Relation
     {
         return app()->get(API::class);
     }
-    
+
     public function hasMany(Endpoint $api, RelationRule $rule, Endpoint $relation, $data, $ids)
     {
         //$builder = DB::table($relation->getTableName());
         $builder = $this->getAPI()->getBuilder($relation);
         //dd($api, $rule, $relation, $data, $ids);
-        
+
         if (!$ids) {
             return null;
         }
-        
+
         $foreignKey = $rule->parameters[1] ?? 'id';
         $ownerKey = $rule->parameters[2] ?? 'id';
         $builder->whereIn($ownerKey, $ids);
         //$builder->where('company_uuid', 'ddd8e28a-4ce9-45ce-b699-828b0bbfd67f');
-        
+
         //dd($ownerKey, $ids);
         //dd($ids, 'asdfa', $ownerKey, $foreignKey);
         if ($relation->soft_deletes) {
@@ -238,19 +238,19 @@ class Relation
                 $query->orWhereNull('deleted_at');
             });
         }
-        
+
         //$builder->limit(25);
-        
+
         $result = $builder->get();
-        
+
         //dd($result, $ownerKey, $ids);
-        
+
         if (!$result) {
             return $result;
         }
-        
+
         //dd($result, $foreignKey, $ownerKey, $relation, $rule, $ids, $builder->toSql());
-        
+
         return $result->keyBy($foreignKey);
     }
 }
